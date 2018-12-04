86f9a99fd2b11a39384d5025caea7f7289bd5493
Frank Faustino <nawzyah@gmail.com>
2018-12-03 19:53:05 -0800

    🚜 Move config files into package.json

--- frontend/__tests__/index.test.js
+++ frontend/__tests__/index.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Index from '../pages/index'
 
-test('', () => {
+test('Basic test', () => {
   const index = shallow(<Index />)
   expect(index.text()).toEqual('Hello 👋')
 })
