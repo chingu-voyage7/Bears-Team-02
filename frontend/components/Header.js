d26e63f0d246da90964722e832a99ad42873c436
Leen Kim <leenkim@alumni.usc.edu>
2018-12-09 22:47:47 -0800

    fix header/search

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Router, { withRouter } from 'next/router'
 import NProgress from 'nprogress'
 import Nav from './Nav'
-// import Search from './Search'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -15,20 +14,19 @@ Router.onRouteChangeError = () => {
   NProgress.done()
 }
 
-const Header = props => (
-  <header className="header">
-    <Link href="/">
-      <a className="logo">
-        <span className="logo__curly">&#123;</span>
-        the_source
-        <span className="logo__curly">&#125;</span>
-      </a>
-    </Link>
-    {/* {props.router !== '/posts' && (
-    <Search handleChange={props.handleChange} client={props.client} />
-    )} */}
-    <Nav />
-  </header>
-)
+const Header = props =>
+  (props.router.pathname === '/' ? (
+    <header className="header">
+      <Link href="/">
+        <a className="logo">
+          <span className="logo__curly">&#123;</span>
+          the_source
+          <span className="logo__curly">&#125;</span>
+        </a>
+      </Link>
 
-export default Header
+      <Nav />
+    </header>
+  ) : null)
+
+export default withRouter(Header)
--- frontend/components/Posts.js
+++ frontend/components/Posts.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ApolloConsumer } from 'react-apollo'
 import gql from 'graphql-tag'
 import debounce from 'lodash.debounce'
-import Search from './Search'
+import InnerHeader from './InnerHeader'
 
 const perPage = 10
 
@@ -94,7 +94,7 @@ class Posts extends React.Component {
       <ApolloConsumer>
         {client => (
           <>
-            <Search handleChange={this.handleChange} client={client} />
+            <InnerHeader handleChange={this.handleChange} client={client} />
             {this.state.loading && <h3>Loading....Do something cool...</h3>}
             <div className="filter">
               {DIFFICULTY.map(d => (
