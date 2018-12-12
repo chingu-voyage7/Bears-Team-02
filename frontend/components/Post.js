56eb3f256295286e1c4dafb1a4f6a9a5f41484e3
Leen Kim <leenkim@alumni.usc.edu>
2018-12-11 23:02:33 -0800

    :sparkles: Add Post component - detail post

--- frontend/components/Post.js
+++ frontend/components/Post.js
@@ -49,30 +49,30 @@ class Post extends React.Component {
         {({ data, loading, error }) => {
           if (loading) return <p>Loading...</p>
           if (error) return <DisplayError error={error} />
-          const { post } = data.post
+
           return (
             <>
               <InnerHeader />
               <div className="post__component" style={{ textDecoration: 'none' }}>
-                <a href={post.href} className="post__href" target="_blank">
+                <a href={data.post.href} className="post__href" target="_blank">
                   <div className="post__image">
-                    <img src={post.image} />
+                    <img src={data.post.image} />
                   </div>
                   <div className="post__detail" style={{ textDecoration: 'none' }}>
-                    <h2 className="post__title">{post.title}</h2>
-                    <p className="post__description">{post.description}</p>
-                    <p className="post__author">Author: {post.author}</p>
-                    <p>Language: {post.language}</p>
-                    <p>Content Type: {post.contentType}</p>
+                    <h2 className="post__title">{data.post.title}</h2>
+                    <p className="post__description">{data.post.description}</p>
+                    <p className="post__author">Author: {data.post.author}</p>
+                    <p>Language: {data.post.language}</p>
+                    <p>Content Type: {data.post.contentType}</p>
                     <p>
                       Tags:
-                      {post.tags.map((tag, i) => (
+                      {data.post.tags.map((tag, i) => (
                         <p key={`${tag}-${i}`}>{`${tag}, `}</p>
                       ))}
                     </p>
-                    <p>Difficulty: {post.difficulty}</p>
-                    <p>{this.displayRating(averageRating(post.reviews))}</p>
-                    <p>{post.price}</p>
+                    <p>Difficulty: {data.post.difficulty}</p>
+                    <p>{this.displayRating(averageRating(data.post.reviews))}</p>
+                    <p>{data.post.price}</p>
                   </div>
                 </a>
               </div>
